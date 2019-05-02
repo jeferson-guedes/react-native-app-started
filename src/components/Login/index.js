479a6c0959d349cf2427cee4b74c56093552b5cc
Jeferson Guedes <jguedes.tec@gmail.com>
2019-05-02 18:29:24 -0300

    Finish layout

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import {StyleSheet, View, Text, Image } from 'react-native';
+import {StyleSheet, View, Text, Image, KeyboardAvoidingView} from 'react-native';
+import LoginForm from './LoginForm';
+
 
 export default class Login extends Component {
     render() {
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
                 <View style={styles.logoContainer}>
-                    <Image source={require('../images/logo.jpg')}/>
+                    <Image 
+                    style={styles.logo}
+                    source={require('../images/Octocat.png')}/>
+                    <Text style={styles.title}> An app made for internal using React Native</Text>
                 </View>
-                <View style={styles.formContainer}></View>
-            </View>
+                <View style={styles.formContainer}>
+                    <LoginForm />
+                </View>
+            </KeyboardAvoidingView>
         );
     }
 }
@@ -17,8 +24,25 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: '#3498db',
     },
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent:'center'
+    }, 
+    logo: {
+        width: 100,
+        height: 100,
+        borderRadius: 5
+    },
+    title: {
+        color: '#FFFFFF',
+        marginTop: 10,
+        width: 160,
+        textAlign: 'center',
+        opacity: 0.9
+    }
 })
